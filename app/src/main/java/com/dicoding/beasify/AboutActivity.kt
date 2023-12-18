c300759585e8a7fcce5a577136417cc43fe08638
SufiyanTs <ah.nursufyantsauri@gmail.com>
2023-12-18 12:35:20 +0700

    first Commit

---
+++ app/src/main/java/com/dicoding/beasify/AboutActivity.kt
@@ -0,0 +1,16 @@
+package com.dicoding.beasify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+
+//    private fun showProgressBar(isLoading: Boolean) {
+//        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+//    }
+}

f1f71ab03c177fc3337ad5a176d8a03062895bdb
Aqiilah Zayyaan Syihab <114908859+AqiilahZayyaan@users.noreply.github.com>
2023-12-22 11:18:13 +0700

    Update index.js

--- Cloud Computing/index.js
+++ Cloud Computing/index.js
@@ -1,34 +1,79 @@
+// index.js
+
 const express = require('express');
-const mysql = require('mysql');
+const database = require('./database');
 const router = express.Router();
+const { Storage } = require('@google-cloud/storage');
+const multer = require('multer');
+const middle = require('./middle');
 
-const app = express();
-app.use(express.json());
+router.get("/input", (_req, res) => {
+    res.send('Hai, this is BEASIFY API !!');
+  });
 
-const connection = mysql.createConnection({
-    host: '34.101.36.126',
-    user: 'root',
-    database: 'excel_beasify',
-    password: 'zxq288fm58'
-});
-
-router.post("/test", (req, res) => {
+  router.post("/input", (req, res) => {
     const { nik, nama, umur, universitas, jurusan, semester, IPK, TOEFL } = req.body;
+    const userquery = 'INSERT INTO data_provider (nik, nama, umur, universitas, jurusan, semester, IPK, TOEFL) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    const uservalues = [nik, nama, umur, universitas, jurusan, semester, IPK, TOEFL];
 
-    const query = "INSERT INTO data_provider (nik, nama, umur, universitas, jurusan, semester, IPK, TOEFL) VALUES (nik, nama, umur, universitas, jurusan, semester, IPK, TOEFL)";
-
-    connection.query(query, [nik, nama, umur, universitas, jurusan, semester, IPK, TOEFL], (err, rows, fields) => {
+    database.query(userquery, uservalues, (err, rows, fields) => {
         if (err) {
-            res.status(500).send({message: err.sqlMessage});
+            console.error('Database error:', err);
+            res.status(500).send({ message: err.sqlMessage });
         } else {
-            res.send({message: "Registration Successful"});
+            res.send({ message: "Input Successful" });
         }
     });
 });
 
-app.use('/', router);
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+const storage = new Storage({
+    keyFilename: 'servicekey.json',
+  });
+  
+  const bucketName = 'beasify_bucket';
+  const bucket = storage.bucket(bucketName);
+  
+  const multerStorage = multer.memoryStorage();
+  const upload = multer({ storage: multerStorage });
+  
+  router.get("/upload-file", (_req, res) => {
+    res.send('Hai, this is BEASIFY API !!');
+  });
+
+  router.post('/upload-file', upload.single('beasifyfile'), async (req, res) => {
+    try {
+      if (!req.file) {
+        return res.status(400).json({ error: 'No file uploaded' });
+      }
+  
+      const { originalname, buffer } = req.file;
+      const userId = req.user.id;
+  
+      const fileName = `${userId}_${Date.now()}_${originalname}`;
+      const file = bucket.file(fileName);
+  
+      const stream = file.createWriteStream({
+        metadata: {
+          contentType: req.file.mimetype,
+        },
+      });
+  
+      stream.on('error', (err) => {
+        console.error('Error uploading to Google Cloud Storage:', err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      });
+  
+      stream.on('finish', async () => {
+        const imageUrl = `https://storage.googleapis.com/${bucketName}/${fileName}`;
+        return res.json({ message: 'Image uploaded successfully', imageUrl });
+      });
+  
+      stream.end(buffer);
+    } catch (error) {
+      console.error('Error processing image upload:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+module.exports = router;
